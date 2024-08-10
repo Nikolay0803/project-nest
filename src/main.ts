84c878fb83dd3d5cb9cb34c19fa152c9f5ba63dc
Mykola_Maslakai <96542705+Nikolay0803@users.noreply.github.com>
2024-08-10 22:10:18 +0300

    Update port main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  await app.listen(5555);
+   const port = process.env.PORT || 5555;
+  await app.listen(port);
 }
 bootstrap();
